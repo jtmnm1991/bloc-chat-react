7fb7c4898f268b3dbcc441855e8cd5b1dfb219ae
Justin <justintmitchell1991@gmail.com>
2018-07-19 12:12:47 -0700

    checkpoint 3 completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,11 +3,21 @@ import React, { Component } from 'react';
 export class RoomList extends Component {
   constructor(props) {
     super(props);
-      this.state = {
-        rooms: []
-      };
+      this.state = {title: "", rooms: []};
 
-    this.roomsRef = this.props.firebase.database().ref('rooms');
+      this.roomsRef = this.props.firebase.database().ref('rooms');
+      this.handleChange = this.handleChange.bind(this);
+      this.createRoom = this.createRoom.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ title: e.target.value });
+  }
+
+  createRoom(e) {
+    e.preventDefault();
+    this.roomsRef.push({ title: this.state.title });
+    this.setState({ title: "" });
   }
 
   componentDidMount() {
@@ -19,11 +29,22 @@ export class RoomList extends Component {
   }
 
   render() {
-    const roomList = this.state.rooms.map((room) =>
-      <li key={room.key}>{room.name}</li>
+    const roomForm = (
+      <form onSubmit={this.createRoom}>
+        <input type="text" value={this.state.title} placeholder="Enter Room Name" onChange={this.handleChange}/>
+        <input type="submit" value="Create" />
+      </form>
     );
+
+    const roomList = this.state.rooms.map((room) =>
+      <li key={room.key}>{room.title}</li>
+    );
+
     return(
-      <ul>{roomList}</ul>
+      <div>
+        <div>{roomForm}</div>
+        <ul>{roomList}</ul>
+      </div>
     );
   }
 }
